96f91c4a3ae10cc1fe49b7c2baa25773cae982d8
Timeman1111 <78430095+Timeman1111@users.noreply.github.com>
2022-09-01 14:30:28 -0400

    Update server.py

--- server.py
+++ server.py
@@ -14,10 +14,6 @@ a continuous flow."""
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
  
-# checks whether sufficient arguments have been provided
-if len(sys.argv) != 3:
-    print ("Correct usage: script, IP address, port number")
-    exit()
  
 # takes the first argument from command prompt as IP address
 IP_address = "45.33.88.39"
