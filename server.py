91639a6d41b025891725f6c5cb6596e386de6f4a
Timeman1111 <78430095+Timeman1111@users.noreply.github.com>
2022-09-12 09:27:41 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import socket
 import threading
 
-HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
+HOST = "45.33.88.39"  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 # conn.sendall(data)
 
